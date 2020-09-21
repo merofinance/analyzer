ef0b46eb1eccd89c66718ad52bd3dcc95550dd7f
xujiahuayz <31636867+xujiahuayz@users.noreply.github.com>
2020-09-21 23:50:43 +0100

    update analysis

--- analysis/markets.py
+++ analysis/markets.py
@@ -2,66 +2,31 @@ import pandas as pd
 import backd.protocols.compound.constants as cnstnts
 from backd import db
 
-liquidations = list(db.db.events.find({"event": "LiquidateBorrow"}))
-
+ctokens = pd.DataFrame(cnstnts.MARKETS)[['address','symbol','decimals']].set_index('address')
 db.db.blocks.delete_one({"timestamp": "timestamp"})
 block_dates = db.get_block_dates()
 
+liquidations = list(db.db.events.find({"event": "LiquidateBorrow"}))
+
+
+def ctoken_sym(addr):
+    return ctokens.loc[str.lower(addr)]['symbol']
+
+def ctoken_dec(addr):
+    return 10**8 #ctokens.loc[str.lower(addr)]['decimals']
+
+
 liquidations_db = pd.DataFrame({
-    'address': this_row['address'],
-    'blockNumber': this_row['blockNumber'],
+    'borrowedToken': ctoken_sym(this_row['address']),
+    'timestamp': block_dates[this_row['blockNumber']],
     'transactionHash': this_row['transactionHash'],
     'liquidator': this_row['returnValues']['liquidator'],
     'borrower': this_row['returnValues']['borrower'],
-    'repayAmount': this_row['returnValues']['repayAmount'],
-    'cTokenCollateral': this_row['returnValues']['cTokenCollateral'],
-    'seizeTokens': this_row['returnValues']['seizeTokens']
+    'repayAmount': int(this_row['returnValues']['repayAmount'])/ctoken_dec(this_row['address']),
+    'collateralToken': ctoken_sym(this_row['returnValues']['cTokenCollateral']),
+    'collateralSeized': int(this_row['returnValues']['seizeTokens'])/ctoken_dec(this_row['returnValues']['cTokenCollateral'])
 } for this_row in liquidations)
 
 
-
-
-# import gzip
-# import json
-# import math
-# import pickle
-# import multiprocessing
-# import csv
-# import glob
-
-# import numpy as np
-# from os import path
-# from datetime import datetime
-# from itertools import groupby
-# from collections import Counter
-# from param import *
-
-# def market_info(file):
-#     print('start', file)
-#     events = []
-#     events_LiquidateBorrow  = []
-#     with gzip.open(file) as f:
-#         for i, row in enumerate(f):
-#             this_row = json.loads(row)
-#             data = {
-#                 'address': this_row['address'],
-#                 'blockNumber': this_row['blockNumber'],
-#                 'event': this_row['event']
-#             }
-#             events.append(data)
-#             # if this_row['event'] == 'LiquidateBorrow':
-#             #    data_LiquidateBorrow = {
-#             #     'blockNumber': this_row['blockNumber'],
-#             #     'transactionHash': this_row['transactionHash'],
-#             #     'liquidator': this_row['returnValues']['liquidator'],
-#             #     'borrower': this_row['returnValues']['borrower'],
-#             #     'repayAmount': this_row['returnValues']['repayAmount'],
-#             #     'cTokenCollateral': this_row['returnValues']['cTokenCollateral'],
-#             #     'seizeTokens': this_row['returnValues']['seizeTokens'],
-#             #    }
-#             #    events_LiquidateBorrow.append(data_LiquidateBorrow)
-#             if i % 10000 == 0:
-#                 print(i)
-#     events_df = pd.DataFrame(events)
-#     events_df_LiquidateBorrow = pd.DataFrame(events_LiquidateBorrow)
-#     return(events_df, events_df_LiquidateBorrow)
+liquidations_db.groupby(['borrowedToken']).sum()
+liquidations_db.groupby(['collateralToken']).sum()['collateralSeized']
