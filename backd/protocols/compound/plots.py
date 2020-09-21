dc06b9cfea1cf9e36a6a84648206bc95b6099ee9
Daniel Perez <daniel@perez.sh>
2020-09-21 19:39:04 +0100

    Add logic to plot over time how close positions are from being liquidated

--- backd/protocols/compound/plots.py
+++ backd/protocols/compound/plots.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 from ... import db
 from .entities import CompoundState
-from .hooks import NonZeroUsers
+from .hooks import NonZeroUsers, UsersBorrowSupply
 
 
 def plot_borrowers_over_time(args: dict):
@@ -21,6 +21,43 @@ def plot_borrowers_over_time(args: dict):
     plt.xlabel("Date")
     plt.ylabel("Number of borrowers")
     plt.plot_date(x, y, fmt="-")
+    plt.tight_layout()
+    output_plot(args.get("output"))
+
+
+def plot_liquidatable_over_time(args: dict):
+    state = CompoundState.load(args["state"])
+    block_dates = db.get_block_dates()
+    users_borrow_supply = state.extra[UsersBorrowSupply.extra_key]
+
+    blocks = [block for block in users_borrow_supply if block in block_dates]
+    x = [block_dates[block] for block in blocks]
+
+    users = users_borrow_supply[blocks[-1]]
+
+    thresholds = [1.0, 1.05, 1.1, 1.25, 1.5, 2.0]
+    labels = ["{0:.2f}%".format(t) for t in thresholds]
+
+    block_buckets = []
+    for block in blocks:
+        users = users_borrow_supply[block]
+        buckets = [0] * len(thresholds)
+        for supply, borrow in users.values():
+            ratio = supply / borrow
+            for i, value in enumerate(thresholds):
+                # only add to first valid bucket
+                if ratio < value:
+                    buckets[i] += borrow / 10 ** 18
+                    break
+        block_buckets.append(buckets)
+
+    ys = list(zip(*block_buckets))
+
+    plt.xticks(rotation=45)
+    plt.xlabel("Date")
+    plt.ylabel("Collateral in USD")
+    plt.stackplot(x, *ys, labels=labels)
+    plt.legend()
     plt.tight_layout()
     output_plot(args.get("output"))
 
